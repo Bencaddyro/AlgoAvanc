b6cc6edaff133211e0b956aeb2fa435ea0a46665
DarkVenus <thierry_constance@hotmail.fr>
2017-03-22 10:16:20 +0100

    Add files via upload

--- dynamique.c
+++ dynamique.c
@@ -1,2 +1,27 @@
+#include <stdio.h>
 
-void soldynamique(int n,point poly[],corde solution[]);
+void soldynamique(int n,point poly[],corde solution[]){
+	var ent i,t,p;
+	int vect[n][n];	//Tableau dans lequel on ne remplira que le triangle sup et contenant les valeurs de Ti,t
+	point souspoly[];	//souspolygone observé
+	for (i = 0, i < n, i++){
+		for(t = 0, t < n, t++){
+			vect[i][t] = 0;
+		}
+	}
+
+
+	for (t = 4, t < n, t++){
+		for (i = 0, i < n, i++){
+			for(p = i, p < t, p++){	
+			//On utilise des sous poligones pour remplire le tableau
+			// pour ce faire on utilise le tableau souspoly contenant uniquement les point du sous polygone définie
+				souspoly[p].x=poly[p].x;
+				souspoly[p].y=poly[p].y;
+				
+				//Calcul du min dans le sous polygone$
+				
+			}
+		}	
+	}
+}
--- makefile
+++ makefile
@@ -9,3 +9,9 @@ glouton.o : glouton.c glouton.h main.h genpoly.h
 
 main : main.c graphique.h graphique.c genpoly.c genpoly.h glouton.o essai.h essai.c dynamique.c dynamique.h
 	gcc -o main main.c graphique.c genpoly.c glouton.o -Wall -lm -lSDL
+
+
+clean:
+	rm -f main
+	rm -f glouton
+	rm -f *.o *.s *.i *.so *~
