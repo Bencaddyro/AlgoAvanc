75456acb2dbab0bc527b1f80dde5a50d51c360f7
DarkVenus <thierry_constance@hotmail.fr>
2017-03-29 22:56:53 +0200

    Add files via upload

--- dynamique.c
+++ dynamique.c
@@ -3,10 +3,10 @@
 #include "genpoly.h"
 
 void soldynamique(int n,point poly[],corde solution[]){
-	int s_deb, s_fin;	//Représentes les sommets de débuts et de fin des sous-polygonnes étudié 
-	int taille = n+2;	//Longeur du tableau tab_sous_poly	
-	piou tab_sous_poly[n][taille];	//On ne remplira que le triangle suppérieur
-	int i,j;
+	//int s_deb, s_fin;	//Représentes les sommets de débuts et de fin des sous-polygonnes étudié 
+	//int taille = n+2;	//Longeur du tableau tab_sous_poly	
+	piou tab_sous_poly[n][n];	//On ne remplira que le triangle suppérieur. 
+	int i,j,k;
 
 	//Initialisation de tab_sous_poly
 	for(i = 0; i < n; i++){
@@ -16,9 +16,9 @@ void soldynamique(int n,point poly[],corde solution[]){
 		c.p2 = poly[i+2];
 		c.lon = coutcorde2(c);
 		//1ère diagonale du tableau
-		tab_sous_poly[i][i+2].somme_corde = c.lon;
-		tab_sous_poly[i][i+2].nb_corde = 1; //On ne regarde qu'une seule corde à cette étape
-		tab_sous_poly[i][i+2].tab_corde[0] = c ;
+		tab_sous_poly[i][i].somme_corde = c.lon;
+		tab_sous_poly[i][i].nb_corde = 1; //On ne regarde qu'une seule corde à cette étape
+		tab_sous_poly[i][i].tab_corde[0] = c ;
 		//Attention pour le tracer des 2 derniéres cordes (Sn-1,S0) et (Sn,S1) d'ou le if
 		if(i <= taille-2){
 			corde2 c;
@@ -26,21 +26,26 @@ void soldynamique(int n,point poly[],corde solution[]){
 			c.p2 = poly[i-n-2]; //TODO: Vérifier l'équation!!!
 			c.lon = coutcorde2(c);
 			//1ère diagonale du tableau
-			tab_sous_poly[i][i+2].somme_corde = c.lon;
-			tab_sous_poly[i][i+2].nb_corde = 1; //On ne regarde qu'une seule corde à cette étape
-			tab_sous_poly[i][i+2].tab_corde[0] = c ;
+			tab_sous_poly[i][i].somme_corde = c.lon;
+			tab_sous_poly[i][i].nb_corde = 1; //On ne regarde qu'une seule corde à cette étape
+			tab_sous_poly[i][i].tab_corde[0] = c ;
 		}
 	}
 
 	//TODO : Remplissage du tableau (par diagonale)
 	for(i = 3; i < n; i++){
 		for(j = 0; j < n; j++){
-			//Création de la corde TODO 
+			//Création de la corde 
 			corde2 c;
 			c.p1 = poly[j];
-			c.p2 = poly[i+j];
+			c.p2 = poly[j+i];
 			c.lon = coutcorde2(c);
+			//TODO:Trouver le min des sous solutions possibles
 			//Remplissage de la diagonale TODO
+			tab_sous_poly[j][i-2].somme_corde = c.lon;
+			tab_sous_poly[j][i-2].nb_corde = i - 1; 
+			tab_sous_poly[j][i-2].tab_corde[0] = c ;
+			//TODO:ATTENTION pour les deux dernières valeurs faire un SI comme plus haut
 			
 		}		
 	}
@@ -54,3 +59,18 @@ typedef struct{
 	int nb_corde;	//Nombres totales de cordes dans le sous-polygone observé
 	corde tab_corde[nb_corde];	//Tableau contenant toutes les cordes retenues
 } piou;*/
+
+
+/*Remarque, voici ce à quoi ressemble tab_sous_poly
+
+	_| 2 | 3 | 4 | 0 | 1
+	0|
+	1|
+	2|
+	3|
+	4|
+	
+
+*/
+
+
